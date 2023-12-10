ec2c3fbee2c126bbbd8b770eed198796191f8a7e
KojimaNaoyuki <programkoji213@gmail.com>
2023-12-10 23:25:41 +0900

    add binding

--- lib/sort_in/model_extension.rb
+++ lib/sort_in/model_extension.rb
@@ -1,10 +1,15 @@
+require'pry'
+
 module SortIn
   module ModelExtension
     extend ActiveSupport::Concern
 
     module ClassMethods
       def sort_in
-        print('aaa')
+        sort_target = all
+        # TODO: in句で指定した条件をどこかしらから取得してsort_targetを並び替える
+        binding.pry
+        'aaa'
       end
     end
   end
--- spec/sort_in_spec.rb
+++ spec/sort_in_spec.rb
@@ -8,8 +8,12 @@ RSpec.describe SortIn do
   class Post < ApplicationRecord; end
 
   describe '.sort_in' do
+    let!(:post_first) { Post.create(title: 'first post', content: 'test') }
+    let!(:post_second) { Post.create(title: 'second post', content: 'hogehoge') }
+    let!(:post_third) { Post.create(title: 'third post', content: 'hogehoge') }
+
     it 'test' do
-      expect(1 + 1).to eq 2
+      expect(Post.where(id: [3, 1, 2]).sort_in).to eq 'aaa'
     end
   end
 end
